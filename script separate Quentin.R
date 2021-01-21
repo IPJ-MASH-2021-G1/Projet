95aa53d6515542196cd1390800f7b1ef70a2ebae
Max0601 <74328172+Max0601@users.noreply.github.com>
2021-01-21 16:44:44 +0100

    Update script separate Quentin.R

--- script separate Quentin.R
+++ script separate Quentin.R
@@ -7,3 +7,8 @@ base_donnees <- read_csv("output.csv", col_types = cols(`Code région` = col_cha
 base_donnees <- separate(base_donnees, `Engagements L. 33-13 et AMEL`,
          sep ="[\\(\\)]",
          into = c("Operateur","Dispositif","Pourcentage_couverture"))
+
+#pour nettoyer en plus commune rurale, commune montagne et zone urbaine
+base_donnees$`Zones très denses`[is.na(base_donnees$`Zones très denses`)] = 0
+base_donnees$`Commune rurale`[is.na(base_donnees$`Commune rurale`)] = 0
+base_donnees$`Commune de montagne`[is.na(base_donnees$`Commune de montagne`)] = 0
